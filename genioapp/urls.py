ae127727d974aee985d8ae928a8b93237fda9494
whichFahim <31901239+whichfahim@users.noreply.github.com>
2023-11-18 16:11:55 -0600

    fixed bug in urls.py

--- genioapp/urls.py
+++ genioapp/urls.py
@@ -1,21 +1,19 @@
 from django.urls import path
 from . import views
 
-app_name = 'genioapp'
+app_name = "genioapp"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('courses/', views.courses, name = 'courses'),
-    path('category/<int:category_no>/', views.detail, name='category_detail'),
-    path('instructorsignup/',views.instructorsignup, name='instructorsignup'),
-    path('courseregistration/',views.courseregistration, name='courseregistration'),
-    path('login/',views.ins_login, name='ins_login'),
-    path('addcourselevels/',views.addcourselevels, name='addcourselevels'),
-    path('viewCourses/',views.viewCourses, name='viewCourses')
-    path('login/redirect/',views.instructorsignup, name='login/redirect/'),
-    path('logout/', views.custom_logout, name='logout'),
-    path('instructor_profile/', views.instructor_profile, name = 'instructor_profile'),
-
-
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("courses/", views.courses, name="courses"),
+    path("category/<int:category_no>/", views.detail, name="category_detail"),
+    path("instructorsignup/", views.instructorsignup, name="instructorsignup"),
+    path("courseregistration/", views.courseregistration, name="courseregistration"),
+    path("login/", views.ins_login, name="ins_login"),
+    path("addcourselevels/", views.addcourselevels, name="addcourselevels"),
+    path("viewCourses/", views.viewCourses, name="viewCourses"),
+    path("login/redirect/", views.instructorsignup, name="login/redirect/"),
+    path("logout/", views.custom_logout, name="logout"),
+    path("instructor_profile/", views.instructor_profile, name="instructor_profile"),
 ]
