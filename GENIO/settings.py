26ed43aebe3b28fc39959af46fd83cab27cfc113
RidhamPatel77 <ridhampatel290@gmail.com>
2023-11-18 14:54:13 -0500

    Ridham | main | minor change

--- GENIO/settings.py
+++ GENIO/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'genioapp.apps.GenioappConfig',
-    'bootstrap5',
+    'django_bootstrap5',
+
+
 ]
 
 MIDDLEWARE = [
