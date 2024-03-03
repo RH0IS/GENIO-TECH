06ea8b369451687525b645de628bf3ed8efd8a0a
officialjunglee <jungleedgr8@gmail.com>
2024-03-02 22:03:23 -0500

    Atishay| added postgres connectivity

--- GENIO/settings.py
+++ GENIO/settings.py
@@ -79,12 +79,23 @@ WSGI_APPLICATION = "GENIO.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'genio-db',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'hostname',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
