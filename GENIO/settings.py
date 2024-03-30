6a0f8b431418f84830c3533bbb30f9ebd6062263
Atishay Khanna <30352985+officialjunglee@users.noreply.github.com>
2024-03-29 23:46:09 -0400

    Update settings.py

--- GENIO/settings.py
+++ GENIO/settings.py
@@ -26,7 +26,7 @@ LOGOUT_REDIRECT_URL = "/login"
 SECRET_KEY = "django-insecure-%u4_5*agghbyh_if67a3-)i(i@%+jxg+wtba!@u)8+(=2l46#="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,22 +82,22 @@ WSGI_APPLICATION = "GENIO.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'genio-db',
+        'USER': 'user123',
+        'PASSWORD': 'admin@123',
+        'HOST': 'genio-postgres-db.postgres.database.azure.com',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'genio-db',
-#         'USER': '{env.DB_USERNAME}',
-#         'PASSWORD': '{env.DB_PASSWORD}',
-#         'HOST': '{env.DB_URL}',
-#         'PORT': '{env.DB_PORT}',  # Default PostgreSQL port
-#     }
-# }
 
 
 
