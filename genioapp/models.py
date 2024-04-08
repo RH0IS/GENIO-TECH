1845aa1c18209b9f52cc09629037e3d36bbcc788
Fahim Imtiaz <fahim.imtiaz07@gmail.com>
2024-04-08 18:17:21 -0400

    fahim | course-level-model | updated description field to text instead of char

--- genioapp/models.py
+++ genioapp/models.py
@@ -57,7 +57,7 @@ class CourseLevels(models.Model):
     name = models.CharField(max_length=100)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2,null=True, default=25)
-    description = models.CharField(max_length=100)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.name
